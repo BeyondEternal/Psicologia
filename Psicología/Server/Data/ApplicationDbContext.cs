0eacb8d786fd763f386e84cddff18197006310f7
BeyondEternal <jojofan0606@gmail.com>
2023-02-15 00:56:08 -0600

    Solved issue

--- Psicología/Server/Data/ApplicationDbContext.cs
+++ Psicología/Server/Data/ApplicationDbContext.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Psicología.Shared.Model;
 
-public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
 
-    public DbSet<Paciente> Pacientes { get; set; }
-    public DbSet<Cita> Citas { get; set; }
-    public DbSet<Terapeuta> Terapeutas { get; set; }
+namespace Psicología.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<Terapeuta> Terapeutas { get; set; }
+    }
 }
